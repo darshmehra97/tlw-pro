bd8aff0edd59d5f70a9ac93f58588075b3d4f0bc
darshmehra97 <darspreet.aim@gmail.com>
2021-02-16 01:59:55 +0530

    commit

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$('.toggle').on('click', function () {
+    $('.navbar-collapse').toggleClass('collapse-nav');
+});
+
+	// $('body').on('click', function (){
+	//      if($(".navbar-collapse").hasClass("collapse-nav")){
+	//      	$('.navbar-collapse').removeClass('collapse-nav');
+	//      }else{
+	     	
+	//      };
+	// });
